b38eddd502b067674fe0283ccfee9a7ac55d6d0e
Paul Tiedtke <PaulTiedtke@web.de>
2023-05-25 18:45:35 +0200

    fix: tailwind not recognizing own code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const { nextui } = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './src/**/*.{js,jsx,ts,tsx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
