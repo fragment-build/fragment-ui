08fbcc7f3e29fb07e5a4ce27eeb890d3519c4ca1
Paul Tiedtke <PaulTiedtke@web.de>
2024-03-19 03:06:55 +0100

    fix: npm package missing exports and jsx not transformed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 export * from '@nextui-org/react';
 
 export * from './components/color-input/ColorInput';
+export * from './components/color-selector/ColorSelector';
+
 export * from './fragments/shell/Shell';
 export * from './fragments/sidebar/Sidebar';
+
+export * from './hooks/breakpoints';
