ea1eda91d831fb6024621ea250f284475a761af0
Paul Tiedtke <PaulTiedtke@web.de>
2024-03-27 22:27:36 +0100

    fix: missing package re-exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "next-themes";
 
+export { getKeyValue, nextui, useDisclosure, VisuallyHidden } from '@nextui-org/react'
+
 export * from './components';
 export * from './fragments';
 export * from './hooks';
