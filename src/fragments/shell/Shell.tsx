a0859806ffd7b8c4788f197077607751d73deef1
Paul Tiedtke <PaulTiedtke@web.de>
2024-06-27 04:52:02 +0200

    feat: center and increase width of main area

--- src/fragments/shell/Shell.tsx
+++ src/fragments/shell/Shell.tsx
@@ -13,7 +13,7 @@ export const Shell: React.FC<ShellProps> = ({ sidebar, children }) => {
   return (
     <div className="flex min-h-screen">
       <Sidebar {...sidebar} />
-      <main className="flex-1 px-6 lg:px-12 xl:px-20 py-14 max-w-screen-2xl min-w-0">
+      <main className="flex-1 px-6 lg:px-12 xl:px-20 py-14 min-w-0 max-w-[1920px] mx-auto">
         {children}
       </main>
     </div>
