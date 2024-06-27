8ba6ba2c2b0211a418d077acd108631683ffdc0e
Paul Tiedtke <PaulTiedtke@web.de>
2024-06-27 02:06:28 +0200

    fix: main layout not shrinking sometimes, typical flexbox issue

--- src/fragments/shell/Shell.tsx
+++ src/fragments/shell/Shell.tsx
@@ -13,7 +13,7 @@ export const Shell: React.FC<ShellProps> = ({ sidebar, children }) => {
   return (
     <div className="flex min-h-screen">
       <Sidebar {...sidebar} />
-      <main className="flex-1 px-6 lg:px-12 xl:px-20 py-14 max-w-screen-2xl">
+      <main className="flex-1 px-6 lg:px-12 xl:px-20 py-14 max-w-screen-2xl min-w-0">
         {children}
       </main>
     </div>
