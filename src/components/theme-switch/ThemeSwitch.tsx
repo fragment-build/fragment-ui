5ea59f601425831d6a578975f2738599e5199b42
Paul Tiedtke <PaulTiedtke@web.de>
2024-06-30 21:39:03 +0200

    fix: theme switch ssr issue

--- src/components/theme-switch/ThemeSwitch.tsx
+++ src/components/theme-switch/ThemeSwitch.tsx
@@ -2,20 +2,27 @@ import { useTheme } from 'next-themes';
 import { IconMoon, IconSun } from '@tabler/icons-react';
 import { Button } from '../base';
 import { ButtonProps } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
 
 /**
  * Primary UI component for selecting a color
  */
 
-export interface ThemeSwitchProps extends ButtonProps {
-}
+export interface ThemeSwitchProps extends ButtonProps {}
 
 export const ThemeSwitch: React.FC<ThemeSwitchProps> = (props) => {
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if(!mounted) return null;
 
   return (
     <Button isIconOnly size="sm" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} {...props}>
       {theme === 'light' ? <IconSun size={18} /> : <IconMoon size={18} />}
     </Button>
-  )
+  );
 };
